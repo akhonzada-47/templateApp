6037a6069e72a532955cf281ccb39bfa630da647
Ghulam Muhammad Akhonzada <80271408+akhonzada-47@users.noreply.github.com>
2024-01-26 15:20:51 +0500

    Update updateUserDto.ts
    add confirm password field in UpdateUserPasswordDto

--- src/user/dtos/updateUserDto.ts
+++ src/user/dtos/updateUserDto.ts
@@ -1,9 +1,14 @@
 import { IsEmail, IsString, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto {
+export class UpdateUserPasswordDto {
   @ApiProperty()
   @IsString()
   @Length(5, 127)
   password: string;
+
+  @ApiProperty()
+  @IsString()
+  @Length(5, 127)
+  confirmPassword: string;
 }
