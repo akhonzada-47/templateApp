268aca366d369f3d4bed6306e9b487c3c1913871
Ghulam Muhammad Akhonzada <80271408+akhonzada-47@users.noreply.github.com>
2024-01-30 11:51:02 +0500

    roleGuardFiles

---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,74 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from '../env.validation';
+
+
+
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector, 
+    private jwtService: JwtService, 
+    private readonly configService: ConfigService<EnvironmentVariables>,
+    ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.get<string>('roles', context.getHandler());
+    
+    if (!roles) {
+      return true; 
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret : this.configService.get('JWT_SECRET')
+        }
+      );
+     
+      request['user'] = payload;
+
+      const user = request.user;
+      return roles.includes(user.role);
+
+    } catch {
+      throw new UnauthorizedException();
+    }
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+  private extractTokenFromHeader(request: any): string | undefined {
+    const authorizationHeader = request.headers.authorization;
+    
+    if (!authorizationHeader) {
+      return authorizationHeader;
+    }
+
+    const [type, token] = authorizationHeader.split(' ');
+
+    return type === 'Bearer' ? token : undefined;
+  }
+  
+
+}
